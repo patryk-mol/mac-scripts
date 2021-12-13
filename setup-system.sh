afb676dfc1ae4ffeac02936cfc5b42ccf0ce6ebe
Patryk Mól <patryk.mol91@gmail.com>
2021-12-13 03:32:23 +0100

    Update setup script

--- setup-system.sh
+++ setup-system.sh
@@ -1,7 +1,47 @@
 #!/bin/zsh
 
-#Disable iTunes backups
+# Disable iTunes backups
 defaults write com.apple.iTunes DeviceBackupsDisabled -bool true
 
-#Enable developer mode
+# Enable developer mode
 sudo /usr/sbin/DevToolsSecurity --enable
+
+# Install brew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> .zprofile
+zsh
+
+brew install \
+  bitwarden \
+  iterm2 \
+  sublime-text \
+  brave-browser \
+  firefox \
+  vscodium \
+  fork \
+  balenaetcher \
+  spotify \
+  vlc \
+  mpv \
+  steam \
+  skype \
+  onlyoffice \
+  midnight-commander \
+  exa \
+  bat \
+  ripgrep \
+  git-delta \
+  bottom \
+  procs \
+  bandwhich \
+  tealdeer \
+  neovim \
+  starship \
+  zsh-autosuggestions \
+  zsh-syntax-highlighting \
+  gzdoom \
+  amethyst
+  
+# Cheat.sh
+curl https://cht.sh/:cht.sh | sudo tee /usr/local/bin/cht.sh
+sudo chmod +x /usr/local/bin/cht.sh
